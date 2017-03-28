88fae67088c4b914aae7e1d2d02eb1e934805b41
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2017-03-28 18:12:14 -0300

    Update installer

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 set -exu
 
-oc -n getup new-app -f observer.yaml --param=CLIENT_NAME=$1 --param=APPS_DOMAIN=$2 --param=HANDLE_EVENT_SLACK_WEBHOOK="$3"
+oc -n getup new-app -f observer.yaml \
+    --param=APPS_DOMAIN=$1 \
+    --param=HANDLE_EVENT_SLACK_WEBHOOK="$2" \
+    ${3:+--params=HANDLE_EVENT_SLACK_CHANNEL="#$3"}
+
 oadm -n getup policy add-cluster-role-to-user cluster-reader system:serviceaccount:getup:observer
